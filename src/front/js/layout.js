3cf1626c2123f468b94b073a2200b83d4cbb4f8d
josewilmerDR <josewdr@gmail.com>
2023-03-19 05:21:54 +0000

    first commit

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -17,30 +17,33 @@ import { Footer } from "./component/footer";
 
 //create your first component
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-    const basename = process.env.BASENAME || "";
+  //the basename is used when your project is published in a subdirectory and not in the root of the domain
+  // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
+  const basename = process.env.BASENAME || "";
 
-    return (
-        <div>
-            <BrowserRouter basename={basename}>
-                <ScrollToTop>
-                    <Navbar />
-                    <Routes>
-                        {/* <Route element={<Home />} path="/" /> */}
-                        {/* <Route element={<ToDo />} path="/" /> */}
-                        <Route element={<StarWars />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<h1>EStoy en la vista de 4Geeks</h1>} path="/4geeks" />
-                        <Route element={<Single />} path="/single/:thetitle" />
-                        <Route element={<SinglePeople />} path="/people/:uid" />
-                        <Route element={<h1>Not found! 404</h1>} path="*" />
-                    </Routes>
-                    <Footer />
-                </ScrollToTop>
-            </BrowserRouter>
-        </div>
-    );
+  return (
+    <div>
+      <BrowserRouter basename={basename}>
+        <ScrollToTop>
+          <Navbar />
+          <Routes>
+            <Route element={<Home />} path="/" />
+            <Route element={<ToDo />} path="/" />
+            <Route element={<StarWars />} path="/" />
+            <Route element={<Demo />} path="/demo" />
+            <Route
+              element={<h1>EStoy en la vista de 4Geeks</h1>}
+              path="/4geeks"
+            />
+            <Route element={<Single />} path="/single/:thetitle" />
+            <Route element={<SinglePeople />} path="/people/:uid" />
+            <Route element={<h1>Not found! 404</h1>} path="*" />
+          </Routes>
+          <Footer />
+        </ScrollToTop>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default injectContext(Layout);
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -3,77 +3,76 @@ import { usuarioStore, usuarioActions } from "./usuario.js";
 import { todoStore, todoActions } from "./todos.js";
 
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			],
-			...exampleStore, //this brings here the variables exampleArray and exampleObject
-			...usuarioStore,
-			...todoStore,
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+  return {
+    store: {
+      message: null,
+      demo: [
+        {
+          title: "FIRST",
+          background: "white",
+          initial: "white",
+        },
+        {
+          title: "SECOND",
+          background: "white",
+          initial: "white",
+        },
+      ],
+      ...exampleStore, //this brings here the variables exampleArray and exampleObject
+      ...usuarioStore,
+      ...todoStore,
+    },
+    actions: {
+      // Use getActions to call a function within a fuction
+      exampleFunction: () => {
+        getActions().changeColor(1, "green");
+      },
+      changeColor: (index, color) => {
+        //get the store
+        const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        //we have to loop the entire demo array to look for the respective index
+        //and change its color
+        const demo = store.demo.map((elm, i) => {
+          if (i === index) elm.background = color;
+          return elm;
+        });
 
-				//reset the global store
-				//setStore({ demo: demo });
+        //reset the global store
+        //setStore({ demo: demo });
 
-				//reset state demo only
-				setStore({ ...store, demo: demo })
-			},
-			...exampleActions(getStore, getActions, setStore), //this will brings here the function exampleFunction, and it will be able to use store's states and actions
-			...usuarioActions(getStore, getActions, setStore),
-			...todoActions(getStore, getActions, setStore),
-			useFetch: async (endpoint, body, method = "GET") => {
-				let url = process.env.BACKEND_URL + endpoint
-				console.log(url)
-				let response = await fetch(url, {
-					method: method,
-					headers: { "Content-Type": "application/json" },
-					body: body ? JSON.stringify(body) : null
-				})
+        //reset state demo only
+        setStore({ ...store, demo: demo });
+      },
+      ...exampleActions(getStore, getActions, setStore), //this will brings here the function exampleFunction, and it will be able to use store's states and actions
+      ...usuarioActions(getStore, getActions, setStore),
+      ...todoActions(getStore, getActions, setStore),
+      useFetch: async (endpoint, body, method = "GET") => {
+        let url = process.env.BACKEND_URL + endpoint;
+        console.log(url);
+        let response = await fetch(url, {
+          method: method,
+          headers: { "Content-Type": "application/json" },
+          body: body ? JSON.stringify(body) : null,
+        });
 
-				let respuestaJson = await response.json()
+        let respuestaJson = await response.json();
 
-				return { respuestaJson, response }
+        return { respuestaJson, response };
+      },
+      useFetchParalelo: (endpoint, body, method = "GET") => {
+        let url = process.env.BACKEND_URL + endpoint;
+        console.log(url);
+        let response = fetch(url, {
+          method: method,
+          headers: { "Content-Type": "application/json" },
+          body: body ? JSON.stringify(body) : null,
+        });
 
-			},
-			useFetchParalelo: (endpoint, body, method = "GET") => {
-				let url = process.env.BACKEND_URL + endpoint
-				console.log(url)
-				let response = fetch(url, {
-					method: method,
-					headers: { "Content-Type": "application/json" },
-					body: body ? JSON.stringify(body) : null
-				})
-
-				return response;
-			},
-		}
-	};
+        return response;
+      },
+    },
+  };
 };
 
 export default getState;
